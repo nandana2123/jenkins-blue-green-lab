ba6652dda407ee0b996a4d295369e671648eb084
nandana2123 <125681336+nandana2123@users.noreply.github.com>
2025-10-21 15:36:32 +0530

    Fix: Correct app port to 8080

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8000;
+const PORT = 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello from Kubernetes! Your CI/CD pipeline is working! 🚀');
